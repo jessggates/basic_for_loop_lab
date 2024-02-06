9f91906288be703f6f8d3383b7a929142b9c018b
Jessica Gates <gatespj@outlook.com>
2024-02-05 21:49:12 -0500

    working code

--- BasicForLoopLab/BasicForLoopLab/Program.cs
+++ BasicForLoopLab/BasicForLoopLab/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Diagnostics.CodeAnalysis;
+
+
+bool playAgain = false;
+int sum = 0;
+int i = 0;
+
+do
+{   
+    Console.Write("Enter a number: ");
+    int userNumber = int.Parse(Console.ReadLine());
+    for (i = 0; i <= userNumber; i++)
+    {
+        sum += i;
+    }
+    Console.WriteLine(sum);
+    Console.WriteLine("Would you like to continue? (y/n?)");
+    string userAnswer = Console.ReadLine().ToLower().Trim();
+    if (userAnswer == "y")
+        playAgain = true;
+    else playAgain = false;
+
+} while (playAgain == true);
+
+
